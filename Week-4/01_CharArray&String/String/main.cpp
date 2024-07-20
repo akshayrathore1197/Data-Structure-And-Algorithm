2e3bf538a2d3e9c6d02dfadb8ee1155466e46a16
Akshay Rathore <akshayrathore2024@gmail.com>
2024-07-20 13:49:36 +0530

    String 1

---
+++ Week-4/01_CharArray&String/String/main.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    // string name = "Maharana Pratap";
+
+    // auto it = name.begin();
+    // while (it != name.end())
+    // {
+    //     cout << *it << " ";
+    //     it++;
+    // }
+    // cout << endl;
+
+    // cout << name[0] << endl;
+    // cout << name.at(0) << endl;
+
+    // cout << name.front() << endl;
+    // cout << name.back() << endl;
+    // cout << name.length() << endl;
+
+    // string sent;
+    // getline(cin, sent);
+    // cout << "Sent : " << sent << endl;
+
+    // string sen;
+    // cin >> sen;
+    // cout << "Sen : " << sen;
+
+    // string str;
+    // str.push_back('a');
+    // str.push_back('k');
+    // str.push_back('s');
+    // str.push_back('h');
+    // str.push_back('a');
+    // str.push_back('y');
+    // cout << "Check : " << str<<endl;
+
+    // str.pop_back();
+    // cout << "Check : " << str<<endl;
+
+    //    string str;
+    //    cout<<"Enter the input : " ;
+    //    cin>>str;
+    //    cout<<"Str : "<<str<<endl;
+    //    cout<<"index : "<<str[0];
+
+    return 0;
+}
