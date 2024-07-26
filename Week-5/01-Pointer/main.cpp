a9a660ce79c77c15782d4f532675d56c0fa478cc
Akshay Rathore <akshayrathore2024@gmail.com>
2024-07-26 12:16:56 +0530

    Pointer 1

---
+++ Week-5/01-Pointer/main.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    // char ch[20] = "akshay";
+    // char *cptr = ch;
+
+    // cout << ch << endl;
+    // cout << &ch << endl;
+    // cout << ch[0] << endl;
+    // cout << &cptr << endl;
+    // cout << *cptr << endl;
+    // cout << *(cptr + 3) << endl;
+    // cout << (cptr) << endl;
+
+    // char ch[100] = "akshayrathore";
+    // cout << &ch << endl;
+
+    // int arr[] = {10, 20, 30, 40};
+    // int *ptr = arr;
+    // ptr = ptr+3;
+    // // arr = arr+3;
+    // cout << "check : " << ptr<<endl;
+    // cout << "check : " << *ptr<<endl;
+    // cout << "check : " << arr+2<<endl;
+
+    // int a = 5;
+    // int *p = &a;
+    // int *q = p;
+
+    // cout << (a) << endl;
+    // cout << (&a) << endl;
+    // // cout << (*a) << endl;
+    // cout << (p) << endl;
+    // cout << (&p) << endl;
+    // cout << (*p) << endl;
+    // cout << (q) << endl;
+    // cout << (&q) << endl;
+    // cout << (*q) << endl;
+
+    // int a = 5;
+    // int *ptr = &a;
+
+    // cout << (a) << endl;
+    // cout << (&a) << endl;
+    // // cout << (*a) << endl;
+    // cout << (ptr) << endl;
+    // cout << (&ptr) << endl;
+    // cout << (*ptr) << endl;
+    // cout << (ptr = ptr + 1) << endl;
+    // cout << (*ptr = (*ptr) / 2) << endl;
+    // cout << (*ptr = *ptr + 1) << endl;
+
+    // int *ptr = 0;
+
+    // int a=7;
+    // int *ptr;
+    // cout << ptr;
+
+    // int a = 5;
+    // int *ptr = &a;
+    // cout << sizeof(ptr) << endl;
+
+    // char ch = 'a';
+    // char *cptr = &ch;
+    // cout << sizeof(cptr) << endl;
+
+    // long l = 1040506433;
+    // long *lptr = &l;
+    // cout << sizeof(lptr) << endl;
+
+    // int a = 5;
+    // int *ptr = &a;
+
+    // cout << a << endl;
+    // cout << &a << endl;
+    // cout << ptr << endl;
+    // cout << &ptr << endl;
+    // cout << *ptr << endl;
+
+    return 0;
+}
